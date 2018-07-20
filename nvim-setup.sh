a7b3f9992fbf475b446c76f558302d2e6df25c77
amin roosta <amin.roosta@outlook.com>
2018-07-20 16:06:39 +0800

    update nvim-setup

--- nvim-setup.sh
+++ nvim-setup.sh
@@ -10,7 +10,7 @@ unzip -x ~/.amin/nvim.zip -d ~/.amin
 (cd ~/.amin/neovim-master && make CMAKE_BUILD_TYPE=Release)
 mkdir -p ~/.amin/neovim-master/nvim
 wget --no-check-certificate -O ~/.amin/neovim-master/nvim/init.vim  https://raw.githubusercontent.com/aminroosta/dot-files/master/.config/nvim/init2.vim
-wget --no-check-certificate -O ~/.ycm_extra_config.py               https://raw.githubusercontent.com/aminroosta/dot-files/master/.ycm_extra_conf.py
+wget --no-check-certificate -O ~/.ycm_extra_conf.py               https://raw.githubusercontent.com/aminroosta/dot-files/master/.ycm_extra_conf.py
 
 pip install neovim
 echo "alias vv='VIMRUNTIME=~/.amin/neovim-master/runtime  ~/.amin/neovim-master/build/bin/nvim -u ~/.amin/neovim-master/nvim/init.vim'" >> ~/.bashrc 
