7b1389bb634cd8ddc20105117c1b032d1c8dcaba
amin roosta <amin.roosta@outlook.com>
2018-07-20 15:09:19 +0800

    add nvim-setup.sh

---
+++ nvim-setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+#mkdir -p ~/.amin
+#wget --no-check-certificate https://github.com/neovim/neovim/archive/master.zip -O ~/.amin/nvim.zip
+
+#apt-get install -y ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip python python-pip
+#unzip -x ~/.amin/nvim.zip -d ~/.amin
+
+#(cd ~/.amin/neovim-master && make CMAKE_BUILD_TYPE=Release)
+#mkdir -p ~/.amin/neovim-master/nvim
+#wget --no-check-certificate -O ~/.amin/neovim-master/nvim/init.vim  https://raw.githubusercontent.com/aminroosta/dot-files/master/.config/nvim/init2.vim
+#wget --no-check-certificate -O ~/.ycm_extra_config.py               https://raw.githubusercontent.com/aminroosta/dot-files/master/.ycm_extra_conf.py
+
+#pip install neovim
+echo "alias vv='VIMRUNTIME=~/.amin/neovim-master/runtime  ~/.amin/neovim-master/build/bin/nvim -u ~/.amin/neovim-master/nvim/init.vim'" >> ~/.bashrc 
+source ~/.bashrc
+
+VIMRUNTIME=~/.amin/neovim-master/runtime  ~/.amin/neovim-master/build/bin/nvim -u ~/.amin/neovim-master/nvim/init.vim -c q "helptags ~/.amin/neovim-master/runtime/doc/"
+VIMRUNTIME=~/.amin/neovim-master/runtime  ~/.amin/neovim-master/build/bin/nvim -u ~/.amin/neovim-master/nvim/init.vim +q
+
+ ~/.amin/neovim-master/nvim/plugged/YouCompleteMe/install.py --clang-completer
