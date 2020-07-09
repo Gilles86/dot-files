85386a05ce85071c38a97f49e8d1cdee921938b8
amin roosta <amin.roosta@outlook.com>
2020-07-09 15:01:27 +0800

    Update install-debian-ag.sh

--- .scripts/install-debian-ag.sh
+++ .scripts/install-debian-ag.sh
@@ -2,8 +2,8 @@
 set -e
 set -x
 
-sudo apt-get install -y automake pkg-config libpcre3-dev zlib1g-dev liblzma-dev
-git clone git@github.com:ggreer/the_silver_searcher.git ~/.ag
+apt-get install -y automake pkg-config libpcre3-dev zlib1g-dev liblzma-dev
+git clone https://github.com/ggreer/the_silver_searcher.git ~/.ag
 cd ~/.ag
 ./build.sh
-sudo make install
+make install
