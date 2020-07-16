519a2d515b024fe0ba9293c26d8a051135baea7a
amin roosta <amin.roosta@outlook.com>
2020-07-16 14:46:42 +0800

    Update install-vim.sh

--- .scripts/install-vim.sh
+++ .scripts/install-vim.sh
@@ -1,21 +1,17 @@
 #!/usr/bin/env bash
-[ `whoami` = root ] || exec su -c $0 root
 set -e
 
 rm -rf ~/.vim-src
 git clone --depth 1 https://github.com/vim/vim.git ~/.vim-src
 
-apt-get install -y libgc-dev
+sudo apt-get install -y libgc-dev libncurses5-dev libncursesw5-dev
 
 cd ~/.vim-src
 ./configure
 
 make -j4
-make install
-mv src/vim /usr/bin/vim
+sudo make install
+sudo mv src/vim /usr/bin/vim
 
 cd -
 rm -rf ~/.vim-src
-
-
-
