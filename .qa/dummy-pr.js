7bf2f2768d62547998848e8234ad71e0a1946925
amin roosta <amin.roosta@outlook.com>
2020-12-29 05:10:32 +0000

    fix

--- .qa/dummy-pr.js
+++ .qa/dummy-pr.js
@@ -17,11 +17,13 @@ const USAGE = `
 `
 var child_process = require('child_process');
 
+// ==> validate arguments
 var branch = process.argv[2];
 var target_repos   = process.argv.slice(3);
 if(!branch || !target_repos.length) { console.warn("\x1b[31m" + USAGE); process.exit(1); }
 if(!process.env.GITHUB_TOKEN) { console.warn("\x1b[31m" + USAGE, "\n GITHUB TOKEN IS MIISING.\n"); process.exit(2); }
 
+// ==> run a blocking command in temrinal
 var run = (command, options) => {
     var quiet = options && options.quiet;
     quiet || console.log(command);
@@ -30,6 +32,7 @@ var run = (command, options) => {
     return res; 
 }
 
+// ==> run a non-blocking command in terminal
 var runAsync = (command, options) => new Promise((resolve, reject) => {
   var quiet = options && options.quiet;
   console.log(command);
@@ -45,23 +48,26 @@ var runAsync = (command, options) => new Promise((resolve, reject) => {
   });
 });
 
+// ==> get the list of all repositories
 var repositories = run('cd /home/git/regentmarkets && ls -d */', {quiet: true})
     .split(/\s+/)
     .map(d => d.replace('/', '')) 
     .filter(d => d);
 
-// git config sets a return code when you ask for a nonexistent config.
+// ==> add "my_remote" as a git remote if it doesn't exist
 var add_remote = (repo) => runAsync(
   `cd /home/git/regentmarkets/${repo} && \
   git config remote.${my_remote}.url >&- || git remote add ${my_remote} git@github.com:${my_github}/${repo}.git && \
   git fetch ${my_remote} && git fetch origin`, {quiet: true}
 );
 
+// ==> returns the "git diff", if the diff is empty it will be marked as a test pr
 var diff_branch = (repo, branch) => (
   runAsync(`cd /home/git/regentmarkets/${repo} && \
     git diff ${my_remote}/${branch} origin/master --stat`, {quiet: true})
 );
 
+// ==> adds the branch if it doesn't already exist
 var add_branch = (repo, branch) => runAsync(
   `cd /home/git/regentmarkets/${repo} && \
   git rev-parse --verify --quiet ${my_remote}/${branch} || \
@@ -69,8 +75,10 @@ var add_branch = (repo, branch) => runAsync(
   git checkout -b ${branch} origin/master`, {quiet: true}
 );
 
+// ==> list of all the pull requests found
 var prs_found = [];
 
+// ==> finds a pr matching the branch and the repo (or returnes undefined).
 var find_pr = (repo, branch) => (
   runAsync(`cd /home/git/regentmarkets/${repo} && hub pr list -h ${my_github}:${branch}`, {quiet: true})
     .then(res => res.split(/\s+|#/)[2])
@@ -83,15 +91,8 @@ var find_pr = (repo, branch) => (
     })
 );
 
-var create_pr_file = (branch) => (
-    Promise.all(
-        repositories.map(
-            repo => find_pr(repo, branch)
-        )
-    ).then(prs => prs.filter(pr => pr))
-    .then(prs => runAsync(`echo '#BEGIN\n${prs.join('\n')}\n#END' > /tmp/prs.txt`))
-);
-
+// ==> commits your local changes, anything already in the "git stash".
+// ==> if there is no change (dummy pr), it creates an empty commit.
 var commit_changes = (repo, branch) => runAsync(
   `cd /home/git/regentmarkets/${repo} && \
   git checkout ${branch} && git pull && \
@@ -99,6 +100,13 @@ var commit_changes = (repo, branch) => runAsync(
   git push -u ${my_remote} ${branch}`
 );
 
+// ==> on first pass, we crete a pr without the notes.
+var create_pr_without_notes = (repo, branch) => runAsync(
+  `cd /home/git/regentmarkets/${repo} && \
+  hub pull-request -m '${branch}' >& /dev/null || echo "Updated PR on ${repo}:${branch}"`
+);
+
+// ==> on the second pass, we update the notes for the pr.
 var create_pr = (repo, branch) => runAsync(
   `cd /home/git/regentmarkets/${repo} && \
   git fetch -f ${my_remote} "refs/notes/pr:refs/notes/pr" && \
@@ -107,27 +115,28 @@ var create_pr = (repo, branch) => runAsync(
   hub pull-request -m '${branch}' >& /dev/null || echo "Updated PR on ${repo}:${branch}"`
 );
 
+// ==> add remote, branch, commit changes and create a pr without notes
 var promise = Promise.all(
     target_repos.map(repo => add_remote(repo)
         .then(() => add_branch(repo, branch))
         .then(() => commit_changes(repo, branch))
+        .then(() => create_pr_without_notes(repo, branch))
     )
-).then(() => Promise.all(
+)
+// ==> then find prs from all repositories, including the ones we just created.
+.then(() => Promise.all(
     repositories.map(
         repo => find_pr(repo, branch)
     ))
-).then(prs => prs.filter(pr => pr));
-
-target_repos.forEach(repo => {
-    promise = promise.then(prs => 
-        runAsync(`echo '#BEGIN\n${prs.join('\n')}\n#END' > /tmp/prs.txt`)
-        .then(() => create_pr(repo, branch))
-        .then(() => find_pr(repo, branch))
-        .then(pr => { prs.push(pr); return prs; })
-    );
-})
-
-promise.then(() => {
+).then(prs => prs.filter(pr => pr))
+// ==> write the git notes file.
+ .then(prs => runAsync(`echo '#BEGIN\n${prs.join('\n')}\n#END' > /tmp/prs.txt`))
+// ==> now update the prs, this time with the notes.
+ .then(() => Promise.all(
+     target_repos.map(repo => create_pr(repo, branch))
+  ))
+// ==> pretty print the result, in a format we can copy to redmine tasks.
+ .then(() => {
     var change_prs = prs_found.filter(pr => pr.endsWith('change')).map(pr => '- ' + pr.split(' ')[0]);
     var test_prs = prs_found.filter(pr => pr.endsWith('test')).map(pr => '- ' + pr.split(' ')[0]);
 
@@ -145,7 +154,6 @@ promise.then(() => {
         '\n# Dummy PRs:\n' + test_prs.join('\n')
     );
     environment && console.log( '\n# environment.yml\n```\n' + environment + '\n```\n');
-
 })['catch'](e => console.error(e));
 
 
